dd735db27f3c9d9ac9749c7cc75bf34560d45c59
Sudhanshu verma <sudhanshuuu01@gmail.com>
2025-06-10 23:35:56 +0530

    Update axios.client.js

--- backend/src/axios/axios.client.js
+++ backend/src/axios/axios.client.js
@@ -1,12 +1,16 @@
 import axios from "axios";
 
 const get = async (url) => {
+  const token = localStorage.getItem("actkn"); // Make sure this matches your localStorage key
+
   const response = await axios.get(url, {
     headers: {
       Accept: "application/json",
-      "Accept-Encoding": "identity"
+      "Accept-Encoding": "identity",
+      Authorization: `Bearer ${token}` // ✅ Add JWT token here
     }
   });
+
   return response.data;
 };
 
