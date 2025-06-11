30875db1e67ed2637216481f5158bca4261e21a5
Sudhanshu verma <sudhanshuuu01@gmail.com>
2025-06-11 11:51:05 +0530

    Update tmdb.config.js

--- backend/src/tmdb/tmdb.config.js
+++ backend/src/tmdb/tmdb.config.js
@@ -1,10 +1,9 @@
-const baseUrl = process.env.TMDB_BASE_URL;
+const baseUrl = process.env.TMDB_BASE_URL; // should be https://api.themoviedb.org/3 (no trailing slash)
 const key = process.env.TMDB_KEY;
 
-const getUrl = (endpoint, params) => {
-  const qs = new URLSearchParams(params);
-
-  return `${baseUrl}${endpoint}?api_key=${key}&${qs}`;
+const getUrl = (endpoint, params = {}) => {
+  const qs = new URLSearchParams(params).toString();
+  return `${baseUrl}/${endpoint}?api_key=${key}${qs ? `&${qs}` : ''}`;
 };
 
 export default { getUrl };
